3fb4e94626178f0e78ac79b741c4e3eea850ffd4
Alaettin Topal <atopal151@posta.pau.edu.tr>
2026-06-07 22:15:32 +0300

    Add FamilyProvider to AppProviders and refactor useFamily hook export

--- src/components/providers/AppProviders.tsx
+++ src/components/providers/AppProviders.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import { AuthProvider } from "@/components/auth/AuthProvider";
+import { FamilyProvider } from "@/components/family/FamilyProvider";
 
 export function AppProviders({ children }: { children: React.ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>;
+  return (
+    <AuthProvider>
+      <FamilyProvider>{children}</FamilyProvider>
+    </AuthProvider>
+  );
 }
